c53d1a114eca790e3411b6f94c7234e8d1e0d6fa
satya740 <satyaprakash74029@gmail.com>
2024-04-04 06:40:49 +0530

    duplicate

---
+++ Duplicate.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class Duplicate {
+    public static boolean DuplicateNumber(int arr[]) {
+       
+        Arrays.sort(arr);
+        for (int i = 0; i < arr.length-1; i++) {
+            if(arr[i]==arr[i+1]){
+                        return true;
+            }
+        }
+        return false;
+
+
+        
+    }
+    public static void main(String[] args) {
+        int arr[] = {1,2,3,8};
+        System.out.println(DuplicateNumber(arr));
+    }
+}
