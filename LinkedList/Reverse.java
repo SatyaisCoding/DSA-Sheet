d707ec2dcc0f52e72aeb3287f20b7bca36d5b7e7
satya740 <satyaprakash74029@gmail.com>
2024-04-08 23:36:46 +0530

    reverseLinkedList

--- LinkedList/Reverse.java
+++ LinkedList/Reverse.java
@@ -1,50 +1,55 @@
-package LinkedList;
+package LinewHeadkedList;
+
 import java.util.*;
 
-class Node{
+class Node {
     int data;
     Node next;
 
-
-    Node(int data, Node next){
+    Node(int data, Node next) {
         this.data = data;
         this.next = next;
     }
-    Node(int data){
-        this.data= data;
-        this.next =null;
+
+    Node(int data) {
+        this.data = data;
+        this.next = null;
     }
 }
 
 public class Reverse {
+    public static Node head;
+    public static Node tail;
 
-    public static Node reverseLinkedList(Node head) {
-        Node temp = head;
-        
+    public static Node reverseLinkedList(Node head) { // Total time Compleexity --> O(n)+O(n) = O(n)
+        Node temp = head; // Space Compleaxity -- > O(n)
+
         Stack<Integer> stack = new Stack<>();
-        while(temp!=null){
-            stack.push(temp.data);   // putting the element into the stack 
-            temp=temp.next;
+        while (temp != null) { // O(n)
+            stack.push(temp.data); // putting the element into the stack
+            temp = temp.next;
         }
-        temp =head;
-        while (temp!=null) {
+        temp = head;
+        while (temp != null) { // O(n)
             temp.data = stack.pop(); // removing the elements from stack in reverse order
-            temp=temp.next;
+            temp = temp.next;
         }
         return head;
     }
+
     public static void printList(Node head) {
 
         Node temp = head;
         while (temp != null) {
-            System.out.print(temp.data + "->");
+            System.out.print(temp.data + "->");   // printing the linked list   eg:  1->2->3->4->5->null
             temp = temp.next;
         }
         System.out.println();
-        
+
     }
+
     public static void main(String[] args) {
-        // 1->2->3->4
+        // 1->2->3->4 -> null
         Node head = new Node(1);
         head.next = new Node(3);
         head.next.next = new Node(2);
@@ -62,5 +67,38 @@ public class Reverse {
         printList(head);
     }
 
+    // 2nd Method
+
+    public void reverseLinkedList2(Node head) {   // TC : O(n)  && SC : O(1)
+        Node curr = tail = head;
+        Node prev = null;
+        Node next;
+                                    //      prev    curr      next 
+        while (curr!=null) {        ///     1   ->   2   ->    3
+            next = curr.next;
+            curr.next = prev;            
+            prev =curr;
+            curr = next;
+        }
+        head = prev;
+    }
+
+
+
+    // 3rd Method 
+
+    public static Node reverseRecursion(Node head) {  // TC : O(n)  && SC : O(1)
+        if(head == null || head.next == null){
+            return head;
+
+        }
+        Node newHead = reverseRecursion(head.next);   //   1->2->3->4->5->null
+                                                     //  1->null
+        Node front = head.next;                      // null<-1<-2 ---> newHead
+        front.next = head;                             // null<-1<-2<-3 ---> newHead
+        head.next = null;                                  // null<-1<-2<-3<-4 ---> newHead
+        return newHead;                                 //// null<-1<-2<-3<-4<-5 ---> newHead
+    }
     
+
 }
