7d9251c1e3aebac4fed9cbed534391449e7b5b1c
satya740 <satyaprakash74029@gmail.com>
2024-07-10 00:30:26 +0530

    pre-in-post-level

--- BinaryTree/BinaryTree.java
+++ BinaryTree/BinaryTree.java
@@ -32,6 +32,62 @@ public class BinaryTree {
         }
 
     }
+    public static void preOrder(Node root){
+        if(root == null){
+            return;
+        }
+
+        System.out.println(root.data+"");
+        preOrder(root.left);
+        preOrder(root.right);
+    }
+    public static void inOrder(Node root){
+        if (root == null) {
+            return;
+        }
+        inOrder(root.left);
+        System.out.println(root.data+"");
+        inOrder(root.right);
+    }
+    public static void postOrder(Node root){
+        if(root== null){
+            return;
+        }
+        preOrder(root.left);
+        preOrder(root.right);
+        System.out.println(root.data+"");
+    }
+
+    public static void levelOrder(Node root){
+        if (root== null) {
+            return;
+        }
+        Queue<Node> q = new LinkedList<>();
+        q.add(root);
+        q.add(null);
+
+        while (!q.isEmpty()) {
+            Node currNode = q.remove();
+
+            if(currNode==null){
+                System.out.println();
+                if (q.isEmpty()) {
+                    break;
+                    
+                }else{
+                    q.add(null);
+                }
+            }else{
+                System.out.println(currNode.data+"");
+                if(currNode.left!=null){
+                    q.add(currNode.left);
+                }
+                if (currNode.right!=null) {
+                    q.add(currNode.right);
+                }
+            }
+        }
+    }
     public static void main(String[] args) {
         int nodes[] = { 1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1 };
         BinaryTree tree = new BinaryTree();
